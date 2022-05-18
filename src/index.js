3f8c000ec89bcff32e2a70dbfde8adadb60edf09
Aran Diez <99818791+AranDiez@users.noreply.github.com>
2022-05-18 07:39:49 +0200

    login and local server working

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
+// DÍA 1 - creamos el servidor con express y hacemos require de lo que necesitamos
+
 const express = require('express');
 const cors = require('cors');
 const moviesFromApi = require('./data/movies.json');
 const usersFromApi = require('./data/users.json');
-// creamos el servidor con express
+
 const server = express();
 
-// configuramos el servidor
+// DÍA 1 -  configuramos el servidor
 server.use(cors());
 server.use(express.json());
-// arrancamos el servidor
+// DÍA 1 -  arrancamos el servidor
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-// Servidor estático que escucha la carpeta publis. es relativo a la raiz del proyecto
+// DÍA 3 -  Servidor estático que escucha la carpeta publis. es relativo a la raiz del proyecto
 const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
 
-// Servidor estático para las fotos
+// DÍA 3 -  Servidor estático para las fotos
 const staticServerImages = './src/public-movies-images';
 server.use(express.static(staticServerImages));
 
@@ -34,11 +36,11 @@ server.use(express.static(staticServerImages));
 //   res.json(response);
 // });
 
-// Primer endpoint que escucha la peticion de películas
-// Filtro por género con query params
-// if params gender le llega vacio devuelve true.
-// else si coincide el gender de la api con el gender que le llega por params lo pone false
-// la respuesta es true con lo que se ha filtrado
+// DÍA 2 -  Primer endpoint que escucha la peticion de películas
+// DÍA 2 -  Filtro por género con query params
+// DÍA 2 -  if params gender le llega vacio devuelve true.
+// DÍA 2 -  else si coincide el gender de la api con el gender que le llega por params lo pone false
+// DÍA 2 -  la respuesta es true con lo que se ha filtrado
 server.get('/movies', (req, res) => {
   const filterMovies = moviesFromApi.filter((movie) => {
     if (req.query.gender == '') {
@@ -52,7 +54,7 @@ server.get('/movies', (req, res) => {
     movies: filterMovies,
   });
 });
-// Peticion por post para comprobar el login de la usuaria
+// DÍA 3 -  Peticion por post para comprobar el login de la usuaria
 server.post('/login', (req, res) => {
   console.log(req.body);
   const loginUsers = usersFromApi.find((user) => {
